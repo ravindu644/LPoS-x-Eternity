bd1fb37766167868698a2845ca10e0e9537cf262
Ravindu Deshan <Droidcasts@protonmail.com>
2024-07-23 23:11:09 +0530

    Enhanced build script and Workflow
    * tmp save 1
    
    * Redesigned: build script
    
    * Adapt workflow
    
    * optimizations
    
    * set up local version
    
    * fixed typo
    
    * tzdev

--- build.sh
+++ build.sh
@@ -1,103 +1,54 @@
 #!/bin/bash
 
 export MODEL=$1
-export piss=$2
+export KSU=$2
 export BUILD_CROSS_COMPILE=$(pwd)/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
-export BUILD_JOB_NUMBER=`grep -c ^processor /proc/cpuinfo`
+export BUILD_JOB_NUMBER=$(grep -c ^processor /proc/cpuinfo)
 RDIR=$(pwd)
 
-case $MODEL in
-beyond2lte)
-    KERNEL_DEFCONFIG=exynos9820-beyond2lte_defconfig
-    SOC=9820
-    BOARD=SRPRI17C014KU
-;;
-beyond1lte)
-    KERNEL_DEFCONFIG=exynos9820-beyond1lte_defconfig
-    SOC=9820
-    BOARD=SRPRI28B014KU
-;;
-beyond0lte)
-    KERNEL_DEFCONFIG=exynos9820-beyond0lte_defconfig
-    SOC=9820
-    BOARD=SRPRI28A014KU
-;;
-beyondx)
-    KERNEL_DEFCONFIG=exynos9820-beyondx_defconfig
-    SOC=9820
-    BOARD=SRPSC04B011KU
-;;
-d1)
-    KERNEL_DEFCONFIG=exynos9820-d1_defconfig
-    SOC=9825
-    BOARD=SRPSD26B007KU
-;;
-d1x)
-    KERNEL_DEFCONFIG=exynos9820-d1xks_defconfig
-    SOC=9825
-    BOARD=SRPSD23A002KU
-;;
-d2s)
-    KERNEL_DEFCONFIG=exynos9820-d2s_defconfig
-    SOC=9825
-    BOARD=SRPSC14B007KU
-;;
-d2x)
-    KERNEL_DEFCONFIG=exynos9820-d2x_defconfig
-    SOC=9825
-    BOARD=SRPSC14C007KU
-;;
+# Device configuration
+declare -A DEVICES=(
+    [beyond2lte]="exynos9820-beyond2lte_defconfig 9820 SRPRI17C014KU"
+    [beyond1lte]="exynos9820-beyond1lte_defconfig 9820 SRPRI28B014KU"
+    [beyond0lte]="exynos9820-beyond0lte_defconfig 9820 SRPRI28A014KU"
+    [beyondx]="exynos9820-beyondx_defconfig 9820 SRPSC04B011KU"
+)
 
-*)
-    echo "Unknown device: $MODEL setting to beyond2lte"
-    KERNEL_DEFCONFIG=exynos9820-beyond2lte_defconfig
-    SOC=9820
-    BOARD=SRPRI17C014KU
-esac
+# Set device-specific variables
+if [[ -v DEVICES[$MODEL] ]]; then
+    read KERNEL_DEFCONFIG SOC BOARD <<< "${DEVICES[$MODEL]}"
+else
+    echo "Unknown device: $MODEL, setting to beyond2lte"
+    read KERNEL_DEFCONFIG SOC BOARD <<< "${DEVICES[beyond2lte]}"
+fi
 
-FUNC_BUILD_KERNEL()
-{
-    echo " Starting a kernel build using "$KERNEL_DEFCONFIG ""
-    # No this is not a typo, samsung left it this way on 12
+#dev
+if [ -z "$LPOS_KERNEL_VERSION" ]; then
+    export LPOS_KERNEL_VERSION="dev"
+fi
+
+#setting up localversion
+echo -e "CONFIG_LOCALVERSION_AUTO=n\nCONFIG_LOCALVERSION=\"-LPoS-x-Eternity-${LPOS_KERNEL_VERSION}\"\n" > "${RDIR}/arch/arm64/configs/version.config"
+
+build_kernel() {
+    local config=$1
+    echo "Starting a kernel build using $KERNEL_DEFCONFIG"
     export PLATFORM_VERSION=11
     export ANDROID_MAJOR_VERSION=r
 
-    if [ -n "$piss" ]; then
-    echo "Building with N variant tzdev"
-    rm -r drivers/misc/tzdev
-    cp -r BTStzdev drivers/misc/tzdev
-    sleep 5
-    fi
+    make -j$BUILD_JOB_NUMBER ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE $KERNEL_DEFCONFIG $config || exit -1
+    make -j$BUILD_JOB_NUMBER ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE menuconfig || true
+    make -j$BUILD_JOB_NUMBER ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE || exit -1
 
-    make -j$BUILD_JOB_NUMBER ARCH=arm64 \
-        CROSS_COMPILE=$BUILD_CROSS_COMPILE \
-        $KERNEL_DEFCONFIG eternity.config || exit -1
-
-    make -j$BUILD_JOB_NUMBER ARCH=arm64 \
-        CROSS_COMPILE=$BUILD_CROSS_COMPILE \
-        menuconfig || true      
-
-    make -j$BUILD_JOB_NUMBER ARCH=arm64 \
-        CROSS_COMPILE=$BUILD_CROSS_COMPILE || exit -1
-
-    $RDIR/toolchains/mkdtimg cfg_create build/dtb_$SOC.img \
-        $RDIR/toolchains/configs/exynos$SOC.cfg \
-        -d $RDIR/arch/arm64/boot/dts/exynos
-
-    echo " Finished kernel build"
+    $RDIR/toolchains/mkdtimg cfg_create build/dtb_$SOC.img $RDIR/toolchains/configs/exynos$SOC.cfg -d $RDIR/arch/arm64/boot/dts/exynos
+    echo "Finished kernel build"
 }
 
-FUNC_BUILD_RAMDISK()
-{
+build_ramdisk() {
     rm -f $RDIR/ramdisk/split_img/boot.img-kernel
     cp $RDIR/arch/arm64/boot/Image $RDIR/ramdisk/split_img/boot.img-kernel
     echo $BOARD > ramdisk/split_img/boot.img-board
-    # This is kinda ugly hack, we could as well touch .placeholder to all of those
-    mkdir -p $RDIR/ramdisk/ramdisk/debug_ramdisk
-    mkdir -p $RDIR/ramdisk/ramdisk/dev
-    mkdir -p $RDIR/ramdisk/ramdisk/mnt
-    mkdir -p $RDIR/ramdisk/ramdisk/proc
-    mkdir -p $RDIR/ramdisk/ramdisk/sys
+    mkdir -p $RDIR/ramdisk/ramdisk/{debug_ramdisk,dev,mnt,proc,sys}
 
     rm -rf $RDIR/ramdisk/ramdisk/fstab.exynos9820
     rm -rf $RDIR/ramdisk/ramdisk/fstab.exynos9825
@@ -108,8 +59,7 @@ FUNC_BUILD_RAMDISK()
     ./repackimg.sh --nosudo
 }
 
-FUNC_BUILD_ZIP()
-{
+build_zip() {
     cd $RDIR/build
     rm -rf $MODEL-boot-ramdisk.img
     mv $RDIR/ramdisk/image-new.img $RDIR/build/$MODEL-boot-ramdisk.img
@@ -123,23 +73,32 @@ FUNC_BUILD_ZIP()
     cp $RDIR/toolchains/updater-script $RDIR/build/zip/META-INF/com/google/android/
     cp $RDIR/toolchains/update-binary $RDIR/build/zip/META-INF/com/google/android/
     cd $RDIR/build/zip
-    zip -r ../LPoS-x-Eternity-${LPOS_KERNEL_VERSION}-${MODEL}${piss}.zip .
+    zip -r ../LPoS-x-Eternity-${LPOS_KERNEL_VERSION}-${MODEL}-${KSU}-universal.zip .
     rm -rf $RDIR/build/zip
     cd $RDIR/build
 }
 
-# MAIN FUNCTION
+# Main execution
 rm -rf ./build.log
 (
-	START_TIME=`date +%s`
+    START_TIME=$(date +%s)
 
-	FUNC_BUILD_KERNEL
-	FUNC_BUILD_RAMDISK
-	FUNC_BUILD_ZIP
+    if [ "$KSU" = "non-ksu" ]; then
+        echo "CONFIG_KSU=n" > "${RDIR}/arch/arm64/configs/ksu.config"
+        build_kernel "eternity.config ksu.config"
+    elif [ "$KSU" = "ksu" ]; then
+        echo "CONFIG_KSU=y" > "${RDIR}/arch/arm64/configs/ksu.config"
+        build_kernel "eternity.config ksu.config"
+    else
+        echo "Error: Invalid input. Please enter 'ksu' or 'non-ksu' as the 2nd parameter"
+        exit 1
+    fi
 
-	END_TIME=`date +%s`
+    build_ramdisk
+    build_zip
 
-	let "ELAPSED_TIME=$END_TIME-$START_TIME"
-	echo "Total compile time was $ELAPSED_TIME seconds"
+    END_TIME=$(date +%s)
+    ELAPSED_TIME=$((END_TIME - START_TIME))
+    echo "Total compile time was $ELAPSED_TIME seconds"
 
-) 2>&1	| tee -a ./build.log
+) 2>&1 | tee -a ./build.log
