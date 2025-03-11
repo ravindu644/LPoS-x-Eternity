76c71d8380b2cefbb82f1352586283a30ef0d9b0
Ravindu Deshan <Droidcasts@protonmail.com>
2025-03-11 06:05:25 +0000

    selinux: Add togglable selinux boot status
    Signed-off-by: Ravindu Deshan <Droidcasts@protonmail.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -217,7 +217,12 @@ static int __init enforcing_setup(char *str)
 {
 	unsigned long enforcing;
 	if (!kstrtoul(str, 0, &enforcing))
+
+	#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 		selinux_enforcing = enforcing ? 1 : 0;
+	#elif defined(CONFIG_SECURITY_SELINUX_ALWAYS_PERMISSIVE)
+		selinux_enforcing = 0;
+	#endif
 
 	return 1;
 }
--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -136,6 +136,12 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	if (sscanf(page, "%d", &new_value) != 1)
 		goto out;
 
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
+ 	new_value = 1;
+#elif defined(CONFIG_SECURITY_SELINUX_ALWAYS_PERMISSIVE)
+ 	new_value = 0;
+#endif
+
 	if (new_value != selinux_enforcing) {
 		length = avc_has_perm(current_sid(), SECINITSID_SECURITY,
 				      SECCLASS_SECURITY, SECURITY__SETENFORCE,
